9d33762b10417450c5732ae09bbe7ec9fbdc1d5d
xandeq <xandeq@gmail.com>
2020-06-22 16:32:19 -0300

    Terceiro

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const express = require('express')
 // inicializa
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("Hello Rocketseat")
+})
+
 // escuta na porta 3001
 app.listen(3001);
